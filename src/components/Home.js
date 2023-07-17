78260ad44078af7f07e8f975b8d45782f9d41f9d
Mohit Jangra <119482895+Mohitjangra489@users.noreply.github.com>
2023-07-17 18:00:15 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,32 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from 'react-router-dom';
+import { useEffect } from 'react';
+
 
 function Home() {
+
+ let navigate=useNavigate();
+  useEffect(()=>{
+    async function check()
+    {
+         fetch(`http://localhost:8000/?token=${JSON.stringify(localStorage.getItem("Token"))}`)             
+        .then(res => res.json())
+        .then(data => { 
+            console.log(data) ;
+            if(data.message='jwt expired')
+            {
+              //  localStorage.clear();
+             navigate('/login');
+            }
+            else
+            {   
+                navigate('/mainpage');
+            }
+        });
+    }
+    check();
+  },[]);
+
   return (
     <div className='homeComponent'>
       <h1 style={{backgroundColor:"white",marginTop:"0px"}}>Welcome to the home page</h1>
